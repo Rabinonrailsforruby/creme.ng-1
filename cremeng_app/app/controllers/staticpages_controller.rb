43dacf2dd29130000d1de1435e46d6f4a91ba570
Rabin Poudyal <rabinonrailsforruby@gmail.com>
2016-07-01 19:41:14 +0545

    Management of some links and taking care of ui elements

---
+++ cremeng_app/app/controllers/staticpages_controller.rb
@@ -0,0 +1,28 @@
+class StaticpagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def terms_of_use
+  end
+
+  def privacy_policy
+  end
+
+  def about_our_stylists
+  end
+
+  def changes_and_cancellation
+  end
+
+  def payment
+  end
+
+  def contact
+  end
+
+  def appointments
+  end
+end
--- cremeng_app/config/routes.rb
+++ cremeng_app/config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  root 'staticpages#home'
 
-  get 'contact' => 'pages#contact'
+  get 'about' => 'staticpages#about'
 
-  get 'about' => 'pages#about'
+  get 'terms_of_use' =>'staticpages#terms_of_use'
+
+  get 'privacy_policy' => 'staticpages#privacy_policy'
+
+  get 'about_our_stylists' => 'staticpages#about_our_stylists'
+
+  get 'changes_and_cancellation' => 'staticpages#changes_and_cancellation'
+
+  get 'payment' => 'staticpages#payment'
+
+  get 'contact' => 'staticpages#contact'
+
+  get 'appointments' => 'staticpages#appointments'
 end
